ee8fca65080cefaa4acec135e815fed69bafc384
Pedro Pupo <pedro.pupo@conpec.com.br>
2019-05-31 03:24:02 -0300

    firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import Firebase from 'Firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4rj24n7jbNRwoS1tEmq1zna67KFFVNRo",
+  authDomain: "gopec-92fb0.firebaseapp.com",
+  databaseURL: "https://gopec-92fb0.firebaseio.com",
+  projectId: "gopec-92fb0",
+  storageBucket: "gopec-92fb0.appspot.com",
+  messagingSenderId: "881721589652",
+  appId: "1:881721589652:web:18e2483f46be9a6e"
+};
+
+firebase = Firebase.initializeApp(firebaseConfig);
+
+export default firebase;
